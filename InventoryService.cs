0d0c7a8a01075a8ce7eba66cdef9e21a3c95f969
Dipali Magar <dipalim680@gmail.com>
2024-05-22 22:43:23 +0530

    Blazor services added

---
+++ InventoryService.cs
@@ -0,0 +1,51 @@
+﻿// Services/InventoryService.cs
+using InventoryBlazor.Models;
+using System.Net.Http.Json;
+
+public class InventoryService
+{
+    private readonly HttpClient _httpClient;
+
+    public InventoryService(HttpClient httpClient)
+    {
+        _httpClient = httpClient;
+    }
+
+    //public async Task<List<InventoryItem>> GetInventoryItemsAsync()
+    //{
+    //    return await _httpClient.GetFromJsonAsync<List<InventoryItem>>("api/inventory");
+    //}
+    public async Task<List<InventoryItem>> GetInventoryItemsAsync()
+    {
+        try
+        {
+            var response = await _httpClient.GetFromJsonAsync<List<InventoryItem>>("api/Inventory");
+            return response;
+        }
+        catch (HttpRequestException ex)
+        {
+            Console.WriteLine($"Error fetching inventory items: {ex.Message}");
+            return new List<InventoryItem>();
+        }
+    }
+
+    public async Task<InventoryItem> GetInventoryItemByIdAsync(int id)
+    {
+        return await _httpClient.GetFromJsonAsync<InventoryItem>($"api/inventory/{id}");
+    }
+
+    public async Task AddInventoryItemAsync(InventoryItem item)
+    {
+        await _httpClient.PostAsJsonAsync("api/inventory", item);
+    }
+
+    public async Task UpdateInventoryItemAsync(InventoryItem item)
+    {
+        await _httpClient.PutAsJsonAsync($"api/inventory/{item.Id}", item);
+    }
+
+    public async Task DeleteInventoryItemAsync(int id)
+    {
+        await _httpClient.DeleteAsync($"api/inventory/{id}");
+    }
+}
---
+++ OrderService.cs
@@ -0,0 +1,47 @@
+﻿using InventoryBlazor.Models;
+using System.Net.Http.Json;
+
+public class OrderService
+{
+    private readonly HttpClient _httpClient;
+
+    public OrderService(HttpClient httpClient)
+    {
+        _httpClient = httpClient;
+    }
+
+    public async Task<List<Order>> GetOrdersAsync()
+    {
+        try
+        {
+            var response = await _httpClient.GetFromJsonAsync<List<Order>>("api/order");
+            return response;
+        }
+        catch (HttpRequestException ex)
+        {
+            Console.WriteLine($"Error fetching orders : {ex.Message}");
+            return new List<Order>();
+        }
+    }
+
+    public async Task<Order> GetOrderByIdAsync(int id)
+    {
+        return await _httpClient.GetFromJsonAsync<Order>($"api/order/{id}");
+
+    }
+
+    public async Task AddOrderAsync(Order order)
+    {
+        await _httpClient.PostAsJsonAsync("api/order", order);
+    }
+
+    public async Task UpdateOrderAsync(Order order)
+    {
+        await _httpClient.PutAsJsonAsync($"api/order/{order.Id}", order);
+    }
+
+    public async Task DeleteOrderAsync(int id)
+    {
+        await _httpClient.DeleteAsync($"api/orders/{id}");
+    }
+}
